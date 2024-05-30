14b939db6bca30cc102b3fbf8d2c7890866f740b
gradx <gradx@users.noreply.github.com>
2024-05-30 17:17:44 -0400

    Delete Kubernetes/rebuild.sh

--- Kubernetes/rebuild.sh
+++
@@ -1,5 +0,0 @@
-az login
-az acr login --name luxurai.azurecr.io
-docker build -t wordpress-nginx -f wordpressNginx.Dockerfile .
-docker tag wordpress-nginx luxurai.azurecr.io/wordpress-nginx:latest
-docker push luxurai.azurecr.io/wordpress-nginx:latest
